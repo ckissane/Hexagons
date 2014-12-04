490c9aa9390b881441028e79dfc67dbdbf7f5bf9
coler706 <coler706@gmail.com>
2014-12-04 12:41:01 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+$(function() {
+$( ".hex" ).prepend( '<div class="inner">' );
+$( ".hex" ).append( "</div>" );
+$( '<div class="corner-1"></div><div class="corner-2"></div>' ).insertAfter( ".inner" );
+});
